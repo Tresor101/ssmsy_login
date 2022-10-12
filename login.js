41f87372efbd6a01cc0f26a969d828dc5ca4690c
Tresor101 <tresorclito1995@gmail.com>
2022-10-12 18:52:02 +0800

    last logged in

--- login.js
+++ login.js
@@ -8,14 +8,13 @@ function authentication(){
             alert ("Login successfully");
         }
         else{
-            // Decrementing by one.
-            logtry++;
-            alert(logtry +"attempted made so far");            
+            // Decrementing by one.            
             if( logtry == 3){
                 document.getElementById("email").disabled = true;
                 document.getElementById("password").disabled = true;
                 document.getElementById("submit").disabled = true;
                 alert(logtry +"attempted made so far");
                 }
+                logtry++; alert(logtry +"attempted made so far");
             }
 }
