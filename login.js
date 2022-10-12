bc5fe21c515d1f461c1919fc0d8b95d6cd786c7a
Tresor101 <tresorclito1995@gmail.com>
2022-10-12 18:44:27 +0800

    last test

--- login.js
+++ login.js
@@ -4,23 +4,18 @@ var logtry = 0;
 function authentication(){
     var username = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    while(logtry <= 3){
         if ( username === "admin@gmail.com" && password === "admin"){
             alert ("Login successfully");
-            break;
         }
         else{
             // Decrementing by one.
             logtry++;
-            //alert(logtry +"attempted made so far");            
+            alert(logtry +"attempted made so far");            
             if( logtry == 3){
                 document.getElementById("email").disabled = true;
                 document.getElementById("password").disabled = true;
                 document.getElementById("submit").disabled = true;
                 alert(logtry +"attempted made so far");
                 }
-                break;
             }
-        //alert(logtry +"attempted made so far");
-        }
 }
